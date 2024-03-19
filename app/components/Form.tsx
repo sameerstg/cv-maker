ac5389f7829c928a2bba60fbc984278a853ad22b
sameerstg <sameerstg@outlook.com>
2024-03-20 02:01:32 +0500

    form state

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,19 +1,26 @@
+'use client'
 import InputField from "@/app/components/InputField";
 import { field } from "@/static/forms";
 
-export default function Form(props: form) {
-  const arr: field[] = props.fieldsWithType;
+export default function Form(props: form, currentForm?: any, setcurrentForm?: any) {
+  function handleNext(e: React.FormEvent) {
+
+    e.preventDefault();
+    console.log("hello world");
+    console.log(currentForm);
+    // setcurrentForm(currentForm + 1);
+  }
   return (
-    <form className="flex w-full flex-col gap-2 justify-center items-start mx-auto my-4">
+    <form className="flex w-full flex-col gap-2 justify-center items-start mx-auto my-4" onSubmit={handleNext}>
 
       <h1 className="text-3xl font-bold text-center mx-auto">{props.formName}</h1>
       {
-        arr.map((field: field,key) => (<>
+        props.fieldsWithType.map((field: field, key) => (<>
 
-          <InputField  key={key} label={field.label} type={field.type} mandatory={field.mandatory} textArea={field.textArea}/>
+          <InputField key={key} label={field.label} type={field.type} mandatory={field.mandatory} textArea={field.textArea} />
         </>))
       }
-      <button className="bg-[#403e3e]  px-8 py-1 rounded-md mx-auto ">Next</button>
+      <button className="bg-[#403e3e]  px-8 py-1 rounded-md mx-auto " type="submit" >Next</button>
 
     </form>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
+'use client'
 import Form, { form } from "@/app/components/Form";
 import RoundedStatus from "@/app/components/RoundedStatus";
-import {allForms} from '@/static/forms'
+import { allForms } from '@/static/forms'
+import { useState } from "react";
 export default function Home() {
-  
+  const [currentForm, setcurrentForm] = useState(0)
   return (
     <>
 
       {
-        allForms.map((form,key) => (
+        allForms.map((form, key) => (
           <RoundedStatus key={key} number={allForms.indexOf(form) + 1} title={form.formName} showLine={allForms.indexOf(form) !== allForms.length - 1} />
         ))
       }
       {
-        allForms.map((form: form,key) => (
-          < Form key={key} formName={form.formName} fieldsWithType={form.fieldsWithType} />
-        ))
+        // allForms.map((form: form,key) => (
+        //   < Form key={key} formName={form.formName} fieldsWithType={form.fieldsWithType} />
+        // ))
+        < Form formName={allForms[currentForm].formName} fieldsWithType={allForms[currentForm].fieldsWithType} />
       }
 
     </>
